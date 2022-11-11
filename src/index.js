4075eba665cf1f784c0f3e07a44a2f82491459e5
ferryalvianto <ferryalvianto@gmail.com>
2022-11-11 14:41:19 -0800

    hashrouter

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import AppMobile from "./AppMobile";
 import {
-  BrowserRouter as Router
+  HashRouter as Router
 } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 if ((navigator.userAgent.match(/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i))) {
   root.render(
-    <Router>
+    <Router basename="/" hashType="noslash">
       <AppMobile />
     </Router>
   )
 } else {
   root.render(
-    <Router>
+    <Router basename="/" hashType="noslash">
       <App />
     </Router>
   );
