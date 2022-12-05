dbacbd7f93857e66a3663e557242c425f7371002
Khang-Truong <khangtruong1995@gmail.com>
2022-12-04 18:04:56 -0800

    change onclick in sentiment

--- src/components/RatingForm.js
+++ src/components/RatingForm.js
@@ -11,17 +11,21 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { display } from '@mui/system';
 import { useNavigate } from 'react-router';
+import axios from 'axios';
 
 const RatingForm = () => {
-	const navigate = useNavigate()
-	const [isMobile, setMobile] = useState(false)
+	// const API_URL = 'https://datalogwebapp.herokuapp.com/datalog/api/'
+	const API_URL = 'http://127.0.0.1:8000/datalog/api/';
+
+	const navigate = useNavigate();
+	const [isMobile, setMobile] = useState(false);
 	const [value, setValue] = useState(5);
 	const StyledRating = styled(Rating)(({ theme }) => ({
 		'& .MuiRating-iconEmpty .MuiSvgIcon-root': {
 			color: theme.palette.action.disabled,
 		},
 	}));
-	const [location, setLocation] = useState('Be Fresh–Cypress')
+	const [location, setLocation] = useState('Be Fresh–Cypress');
 
 	const customIcons = {
 		1: {
@@ -61,92 +65,182 @@ const RatingForm = () => {
 	};
 
 	useEffect(() => {
-		if ((navigator.userAgent.match(/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i))) {
-			setMobile(true)
-			document.getElementById('formdiv').style.height = 'auto'
+		if (
+			navigator.userAgent.match(
+				/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i
+			)
+		) {
+			setMobile(true);
+			document.getElementById('formdiv').style.height = 'auto';
 		} else {
-			setMobile(false)
-			document.getElementById('formdiv').style.height = '95vh'
-
+			setMobile(false);
+			document.getElementById('formdiv').style.height = '95vh';
 		}
-	}, [])
+	}, []);
 
 	return (
 		<>
-			<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }} id='formdiv'>
+			<div
+				style={{
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+					flexDirection: 'column',
+				}}
+				id="formdiv"
+			>
+				<div className="patternDiv"></div>
 
-				<div className='patternDiv'></div>
+				{isMobile ? (
+					<div
+						className="card"
+						id="cardBackground"
+						style={{
+							width: '100vw',
+							display: 'flex',
+							alignItems: 'center',
+							cursor: 'pointer',
+						}}
+						onClick={(e) => {
+							// window.open('https://www.befresh.ca/', '_blank')
+						}}
+					>
+						<img
+							src={
+								'https://www.befresh.ca/wrdp/wp-content/uploads/2017/12/BeFresh_Web_LogoArtboard-1@2x.png'
+							}
+							style={{ height: 'auto', width: '100%', filter: 'invert(1)' }}
+						/>
+					</div>
+				) : (
+					<div
+						className="card"
+						id="cardBackground"
+						style={{
+							width: '90vw',
+							display: 'flex',
+							alignItems: 'center',
+							cursor: 'pointer',
+						}}
+						onClick={(e) => {
+							// window.open('https://www.befresh.ca/', '_blank')
+						}}
+					>
+						<img
+							src={
+								'https://www.befresh.ca/wrdp/wp-content/uploads/2017/12/BeFresh_Web_LogoArtboard-1@2x.png'
+							}
+							style={{ height: 'auto', width: '40%', filter: 'invert(1)' }}
+						/>
+					</div>
+				)}
 
-				{isMobile ?
-					(
-						<div className='card' id='cardBackground' style={{ width: '100vw', display: 'flex', alignItems: 'center', cursor: 'pointer' }} onClick={(e) => {
-							// window.open('https://www.befresh.ca/', '_blank') 
-						}}>
-							<img src={"https://www.befresh.ca/wrdp/wp-content/uploads/2017/12/BeFresh_Web_LogoArtboard-1@2x.png"} style={{ height: 'auto', width: '100%', filter: 'invert(1)' }} />
-						</div>
-
-					) : (
-						<div className='card' id='cardBackground' style={{ width: '90vw', display: 'flex', alignItems: 'center', cursor: 'pointer' }} onClick={(e) => {
-							// window.open('https://www.befresh.ca/', '_blank') 
-						}}>
-							<img src={"https://www.befresh.ca/wrdp/wp-content/uploads/2017/12/BeFresh_Web_LogoArtboard-1@2x.png"} style={{ height: 'auto', width: '40%', filter: 'invert(1)' }} />
-						</div>
-					)}
-
-				<div className='card' id='cardBackground' style={{ width: '100vw', padding: '2rem' }}>
+				<div
+					className="card"
+					id="cardBackground"
+					style={{ width: '100vw', padding: '2rem' }}
+				>
 					<Form>
-						<Form.Group controlId="exampleForm.ControlTextarea1" className='text-start'>
-							{isMobile ? (<>
-								<h2 style={{ marginTop: '1rem', marginBottom: '1rem' }}>How was your experience at our store?</h2>
-								<div style={{ marginTop: '1rem', marginBottom: '1rem',  width: '90vw', display: 'flex', alignItems: 'center' }}
-								>
-									<StyledRating
-										name="rating"
-										value={value}
-										onChange={(event, newValue) => {
-											setValue(newValue);
+						{/* <Form.Group */}
+							{/* controlId="exampleForm.ControlTextarea1"
+							className="text-start"
+						> */}
+							{isMobile ? (
+								<>
+									<h2 style={{ marginTop: '1rem', marginBottom: '1rem' }}>
+										How was your experience at our store?
+									</h2>
+									<div
+										style={{
+											marginTop: '1rem',
+											marginBottom: '1rem',
+											width: '90vw',
+											display: 'flex',
+											alignItems: 'center',
 										}}
-										IconContainerComponent={IconContainer}
-										getLabelText={(value) => customIcons[value].label}
-										highlightSelectedOnly
-									/>
-								</div>
-							</>) : (<>
-								<div style={{ display: 'flex', marginTop: '1rem', marginBottom: '1rem', alignItems: 'center' }}>
-									<h2 style={{ marginTop: '1rem', marginBottom: '1rem', width: '50%' }}>How was your experience at our store?</h2>
-									<StyledRating
-										name="rating"
-										value={value}
-										onChange={(event, newValue) => {
-											setValue(newValue);
+									>
+										<StyledRating
+											name="rating"
+											value={value}
+											onChange={(event, newValue) => {
+												setValue(newValue);
+											}}
+											IconContainerComponent={IconContainer}
+											getLabelText={(value) => customIcons[value].label}
+											highlightSelectedOnly
+										/>
+									</div>
+								</>
+							) : (
+								<>
+									<div
+										style={{
+											display: 'flex',
+											marginTop: '1rem',
+											marginBottom: '1rem',
+											alignItems: 'center',
 										}}
-										IconContainerComponent={IconContainer}
-										getLabelText={(value) => customIcons[value].label}
-										highlightSelectedOnly
-										style={{ marginTop: '1rem', marginBottom: '1rem', width: '50%' }}
-									/>
-								</div>
-							</>)}
+									>
+										<h2
+											style={{
+												marginTop: '1rem',
+												marginBottom: '1rem',
+												width: '50%',
+											}}
+										>
+											How was your experience at our store?
+										</h2>
+										<StyledRating
+											name="rating"
+											value={value}
+											onChange={(event, newValue) => {
+												setValue(newValue);
+											}}
+											IconContainerComponent={IconContainer}
+											getLabelText={(value) => customIcons[value].label}
+											highlightSelectedOnly
+											style={{
+												marginTop: '1rem',
+												marginBottom: '1rem',
+												width: '50%',
+											}}
+										/>
+									</div>
+								</>
+							)}
 
-							{isMobile ? (<>
-								<h2 style={{ marginTop: '1rem', marginBottom: '1rem' }}>Select location:</h2>
-								<div style={{ marginTop: '1rem', marginBottom: '1rem' }}>
+							{isMobile ? (
+								<>
+									<h2 style={{ marginTop: '1rem', marginBottom: '1rem' }}>
+										Select location:
+									</h2>
+									{/* <div style={{ marginTop: '1rem', marginBottom: '1rem' }}>
 									<select className='sel' onChange={(e) => { setLocation(e.target.value) }}>
 										<option name="CY">Be Fresh–Cypress</option>
 										<option name="OA">Organic Acres–Main St.</option>
 									</select>
-								</div>
-							</>) : (<>
-								<div style={{ display: 'flex', marginTop: '1rem', marginBottom: '1rem', alignItems: 'center' }}>
-									<h2 style={{ width: '50%' }}>Select location:</h2>
-									<div style={{ width: '50%' }}>
+								</div> */}
+								</>
+							) : (
+								<>
+									<div
+										style={{
+											display: 'flex',
+											marginTop: '1rem',
+											marginBottom: '1rem',
+											alignItems: 'center',
+										}}
+									>
+										<h2 style={{ width: '50%' }}>Select location:</h2>
+										{/* <div style={{ width: '50%' }}>
 										<select className='sel' onChange={(e) => { setLocation(e.target.value) }}>
 											<option name="CY">Be Fresh–Cypress</option>
 											<option name="OA">Organic Acres–Main St.</option>
 										</select>
+									</div> */}
 									</div>
-								</div>
-							</>)}
+								</>
+							)}
 							<Form.Control
 								as="textarea"
 								name="comment"
@@ -154,21 +248,47 @@ const RatingForm = () => {
 								placeholder="Enter your feedback here 􀌲"
 								value={textArea}
 								onChange={handleChange}
-								style={{ padding: '1rem', marginTop: '1rem', marginBottom: '1rem' }}
+								style={{
+									padding: '1rem',
+									marginTop: '1rem',
+									marginBottom: '1rem',
+								}}
 							/>
-						</Form.Group>
+						{/* </Form.Group> */}
 						<div style={{ marginTop: '1rem', marginBottom: '1rem' }}></div>
-						<Button variant="primary" type="submit" className={`btn bttn`}>
-							Submit
-						</Button>
+						<div
+							onClick={(e) => {
+								// 	// e.preventDefault();
+								console.log('clicked');
+								axios
+									.get(
+										'http://127.0.0.1:8000/datalog/api/' +
+											'insert_sentiments/?db=' +
+											JSON.parse(localStorage.getItem('user')).db.toString() +
+											'&rating=' +
+											value +
+											'&comment=' +
+											textArea
+									)
+									.then((res) => {});
+							}}
+						>
+							<Button variant="primary" className={`btn bttn`}>
+								Submit
+							</Button>
+						</div>
 					</Form>
 				</div>
-				<div style={{ marginTop: '1rem', marginBottom: '1rem', cursor: 'pointer' }} onClick={(e) => { window.open('/', '_blank') }}>
-					<h5>Analysis by DataLog</h5 >
+				<div
+					style={{ marginTop: '1rem', marginBottom: '1rem', cursor: 'pointer' }}
+					onClick={(e) => {
+						window.open('/', '_blank');
+					}}
+				>
+					<h5>Analysis by DataLog</h5>
 				</div>
 			</div>
 		</>
-
 	);
 };
 
