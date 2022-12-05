5e2e846b04b72234a78cb87bc3353b78e58bc4a4
Khang-Truong <khangtruong1995@gmail.com>
2022-12-05 14:31:39 -0800

    delete Math.random in leafletData.js

--- src/TempData/LeafletData.js
+++ src/TempData/LeafletData.js
@@ -1,211 +1,133 @@
 export const leafletData = [
-  {
+	{
 		name: 'Citruses',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +23.45,
-			Math.ceil(Math.random() * 0) +60,
-		],
+		coordinates: [23.45, 60],
 		quantity: 94,
 	},
-  {
+	{
 		name: 'Avocados',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +25,
-			Math.ceil(Math.random() * 0) +60,
-		],
+		coordinates: [25, 60],
 		quantity: 8,
 	},
 
-  {
+	{
 		name: 'Apples',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +27,
-			Math.ceil(Math.random() * 0) +60,
-		],
+		coordinates: [27, 60],
 		quantity: 47,
 	},
-  {
+	{
 		name: 'Yams and Potatoes',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +23.45,
-			Math.ceil(Math.random() * 0) +62,
-		],
+		coordinates: [23.45, 62],
 		quantity: 19,
 	},
-  {
+	{
 		name: 'Garlic and Onions',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +27,
-			Math.ceil(Math.random() * 0) +62,
-		],
+		coordinates: [27, 62],
 		quantity: 20,
 	},
 	{
 		name: 'Bananas',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +45,
-			Math.ceil(Math.random() * 0) +50,
-		],
+		coordinates: [45, 50],
 		quantity: 11,
 	},
 	{
 		name: 'Produce',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +20,
-			Math.ceil(Math.random() * 0) +60,
-		],
+		coordinates: [20, 60],
 		quantity: 11,
 	},
 	{
 		name: 'Coffee Bar',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +45.23,
-			Math.ceil(Math.random() * 0) +45,
-		],
+		coordinates: [45.23, 45],
 		quantity: 152,
 	},
 	{
 		name: 'Breads',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +26,
-			Math.ceil(Math.random() * 0) +70,
-		],
+		coordinates: [26, 70],
 		quantity: 122,
 	},
 	{
 		name: 'Hardbite',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +30,
-			Math.ceil(Math.random() * 0) +35,
-		],
+		coordinates: [30, 35],
 		quantity: 21,
 	},
 	{
 		name: 'Chocolates',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +45,
-			Math.ceil(Math.random() * 0) +40,
-		],
+		coordinates: [45, 40],
 		quantity: 30,
 	},
 	{
 		name: 'Snack Bars',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +30,
-			Math.ceil(Math.random() * 0) +43,
-		],
+		coordinates: [30, 43],
 		quantity: 35,
 	},
 	{
 		name: 'Chips',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +23.45,
-			Math.ceil(Math.random() * 0) +40,
-		],
+		coordinates: [23.45, 40],
 		quantity: 173,
 	},
 	{
 		name: 'Health & Beauty',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +32,
-			Math.ceil(Math.random() * 0) +40,
-		],
+		coordinates: [32, 40],
 		quantity: 85,
 	},
 	{
 		name: 'Dairy',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +26.23,
-			Math.ceil(Math.random() * 0) +45,
-		],
+		coordinates: [26.23, 45],
 		quantity: 186,
 	},
 	{
 		name: 'Dips and Produce',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +20,
-			Math.ceil(Math.random() * 0) +40,
-		],
+		coordinates: [20, 40],
 		quantity: 272,
 	},
 	{
 		name: 'Oils',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +26,
-			Math.ceil(Math.random() * 0) +45,
-		],
+		coordinates: [26, 45],
 		quantity: 3,
 	},
 	{
 		name: 'Broths',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +26,
-			Math.ceil(Math.random() * 0) +40,
-		],
+		coordinates: [26, 40],
 		quantity: 11,
 	},
 	{
 		name: 'Coffee and Teas',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +45.23,
-			Math.ceil(Math.random() * 0) +55.23,
-		],
+		coordinates: [45.23, 55.23],
 		quantity: 11,
 	},
 	{
 		name: 'Peanut Butters',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +28,
-			Math.ceil(Math.random() * 0) +45,
-		],
+		coordinates: [28, 45],
 		quantity: 6,
 	},
 	{
 		name: 'Pastas',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +26,
-			Math.ceil(Math.random() * 0) +45,
-		],
+		coordinates: [26, 45],
 		quantity: 3,
 	},
 	{
 		name: 'Frozen meat and seafood',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +28.23,
-			Math.ceil(Math.random() * 0) +45,
-		],
+		coordinates: [28.23, 45],
 		quantity: 101,
 	},
 	{
 		name: 'Vegan',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +23.45,
-			Math.ceil(Math.random() * 0) +70,
-		],
+		coordinates: [23.45, 70],
 		quantity: 11,
 	},
 	{
 		name: 'Candies',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +43,
-			Math.ceil(Math.random() * 0) +40,
-		],
+		coordinates: [43, 40],
 		quantity: 11,
 	},
 	{
 		name: 'Local Items',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +23.45,
-			Math.ceil(Math.random() * 0) +23.45,
-		],
+		coordinates: [23.45, 23.45],
 		quantity: 11,
 	},
 	{
 		name: 'Deli and Cheese and Meals',
-		coordinates: [
-			Math.ceil(Math.random() * 0) +50,
-			Math.ceil(Math.random() * 0) +40,
-		],
+		coordinates: [50, 40],
 		quantity: 105,
 	},
 ];
